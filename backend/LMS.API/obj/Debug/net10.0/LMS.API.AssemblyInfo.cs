55bfbe34927a4737918f64002d33c49d42b6ffc5
SneiderM2 <neigamer0125@gmail.com>
2026-09-23 15:00:43 -0500

    3.0

--- backend/LMS.API/obj/Debug/net10.0/LMS.API.AssemblyInfo.cs
+++ backend/LMS.API/obj/Debug/net10.0/LMS.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d19e1c35c590842a7323ff941cebc4bfeb9cf1eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
